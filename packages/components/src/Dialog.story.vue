5b894744edb7ed4db3f338893bc479e0ecdb219f
maxblom123 <maxacblom@proton.me>
2026-07-28 19:37:08 +0200

    dialog

---
+++ packages/components/src/Dialog.story.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Dialog from './Dialog.vue'
+
+const isDefaultOpen = ref(false)
+const isFooterOpen = ref(false)
+const isEmptyOpen = ref(false)
+</script>
+
+<template>
+  <Story title="Dialog">
+    <Variant title="Default">
+      <button type="button" @click="isDefaultOpen = true">Open dialog</button>
+      <Dialog v-model="isDefaultOpen" title="Confirm action">
+        <p>Are you sure you want to continue?</p>
+        <button type="button" @click="isDefaultOpen = false">Close</button>
+      </Dialog>
+    </Variant>
+
+    <Variant title="With footer slot">
+      <button type="button" @click="isFooterOpen = true">Open dialog</button>
+      <Dialog v-model="isFooterOpen" title="Delete item">
+        <p>This action cannot be undone.</p>
+        <template #footer>
+          <button type="button" @click="isFooterOpen = false">Cancel</button>
+          <button type="button" @click="isFooterOpen = false">Delete</button>
+        </template>
+      </Dialog>
+    </Variant>
+
+    <Variant title="No focusable content">
+      <button type="button" @click="isEmptyOpen = true">Open dialog</button>
+      <Dialog v-model="isEmptyOpen" title="Loading">
+        <p>Please wait, this will close automatically.</p>
+      </Dialog>
+    </Variant>
+  </Story>
+</template>
